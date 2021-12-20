c4f9aff0186615a9c991ee2c859ac1ea7b63ff96
Azer154 <93781934+Azer154@users.noreply.github.com>
2021-12-20 16:22:29 +0400

    Add files via upload

---
+++ covid.js
@@ -0,0 +1,53 @@
+console.log('Processing...')
+const puppeteer = require('puppeteer')
+const moment = require('moment')
+async function scrapeProduct(url) {
+    const st = performance.now()
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setDefaultNavigationTimeout(60000);
+    await page.goto(url)
+
+    let [web_newc] = await page.$x('//*[@id="main_table_countries_today"]/tbody[1]/tr[1]/td[4]')
+    let newc = await web_newc.getProperty('textContent')
+    let new_cases = await newc.jsonValue()
+    console.log('✅ | Loaded New Cases')
+
+    let [web_newdeaths] = await page.$x('//*[@id="main_table_countries_today"]/tbody[1]/tr[1]/td[6]')
+    let newd = await web_newdeaths.getProperty('textContent')
+    let new_d = await newd.jsonValue()
+    console.log('✅ | Loaded New Deaths')
+    let [web_newr] = await page.$x('//*[@id="main_table_countries_today"]/tbody[1]/tr[1]/td[8]')
+    let newr = await web_newr.getProperty('textContent')
+    let new_r = await newr.jsonValue()
+    console.log('✅ | Loaded New Recoveries')
+    let [web_ac] = await page.$x('//*[@id="main_table_countries_today"]/tbody[1]/tr[1]/td[9]')
+    let ac = await web_ac.getProperty('textContent')
+    let a_c = await ac.jsonValue()
+    console.log('✅ | Loaded Active Cases')
+    let [web_tr] = await page.$x('//*[@id="main_table_countries_today"]/tbody[1]/tr[1]/td[7]')
+    let webtr = await web_tr.getProperty('textContent')
+    let t_r = await webtr.jsonValue()
+    console.log('✅ | Loaded Total Recoveries')
+    let [web_tc] = await page.$x('//*[@id="main_table_countries_today"]/tbody[1]/tr[1]/td[3]')
+    let webtc = await web_tc.getProperty('textContent')
+    let t_c = await webtc.jsonValue()
+    console.log('✅ | Loaded Total Cases')
+    let table = {
+        "new_cases": new_cases,
+        "new_recoveries": new_r,
+        "new_deaths": new_d,
+        "active_cases": a_c,
+        "total_recoveries": t_r,
+        "total_cases": t_c 
+
+    }
+    console.table(table)
+let et = performance.now()
+ et = moment(st - et).seconds()
+console.log(`${et} seconds taken`)
+
+
+    await browser.close()
+}
+scrapeProduct('https://www.worldometers.info/coronavirus/')
